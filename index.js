4419161b8816dd3a56db029f7a798da6cbfaced2
farzan190 <farzanxavier@gmail.com>
2024-09-22 13:11:37 +0530

    each lift is moving per floor

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const handleLogicperLift=0
+let liftState=[];
 let number=1;
+
+liftState=[];
+[...Array(4)].forEach((e,index) => {
+    const eachliftDetail={
+        "id":index+1,
+        "state":"idle",
+        "currentFloor":0,
+    }
+ liftState.push(eachliftDetail);
+});  
 for(let i=16;i>0;i--){
     const newElement = document.createElement('div'); 
     newElement.innerHTML = ` 
@@ -20,40 +31,57 @@ for(let i=16;i>0;i--){
     
     for(let i=1;i<5;i++){
         const newElement = document.createElement('div'); 
-        newElement.innerHTML = `<h1 class="box" id=${i} ></h1> `;
-    
+        newElement.innerHTML = `<h1 class="box" id=${i}  ></h1> `; 
         const newLifts=document.querySelector('.elevators');
          
         newLifts.appendChild(newElement); 
-          
-        
         }
         
-    const handleMovement=(e)=>{
-    console.log(e);
+
+    const handleMovement=(floorcalled)=>{
      if(number>4)
         number=1;
-    const lifts= document.querySelector(`.elevators`);
 
-    const query = lifts.querySelector(`[id="${number}"]`);
     
-    query.style.transform= `translateY(-${e*100}px)`;
-    query.style.transition=`transform 2s ease`;
+
+    for(let i=0;i<liftState.length;i++){
+    if (liftState[i].currentFloor==floorcalled){     // 
+          return  ;
+    }}
     
-    number++;
-     
 
-    }
+    const lifts= document.querySelector(`.elevators`); 
+    const singleLift = lifts.querySelector(`[id="${number}"]`);
 
+    singleLift.addEventListener('click',liftMovementTrack(number,"moving",floorcalled)); 
+    
+    singleLift.style.transform= `translateY(-${floorcalled*100}px)`;  
+    singleLift.style.transition=`transform 2s ease`; 
+    setTimeout (()=>{
+    liftMovementTrack(number,"idle",floorcalled);
+   },2000);
+   number++;  
+  
+
+}
+
+
+   const liftMovementTrack=(liftnumber,state,currentFloor)=>{
+    const updatedLift = liftState.map((lift) => {
+        if (lift.id === liftnumber) {
+          return {
+            ...lift,               
+            state: state,          
+            currentFloor: currentFloor 
+          };
+        }
+        return lift; 
+      });
+       liftState=updatedLift;
+      console.log(updatedLift);
+
+   }
    
-   
 
-    
-    // if you press updown then lift moves there 
-    // agar ye naya floor hai toh same will happen if these existing two are in transition 
-    //  if not then these will move to the floor where lifts get called 
-    
-    
-    
-      // transform:  translateY(+500px) ;
-       // abhi sabse bada task ye hai ki lift kese move karani hai  
+                                    
+  
