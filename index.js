d96b007be63d9a31c0db724558cf02f9ceae53ed
farzan190 <farzanxavier@gmail.com>
2024-09-24 17:24:46 +0530

    implemented two lifts per floor ,One for each button

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const handleLogicperLift=0
 let liftState=[];
 let number=1;
+let count=0;
 
 liftState=[];
 [...Array(4)].forEach((e,index) => {
@@ -8,6 +9,7 @@ liftState=[];
         "id":index+1,
         "state":"idle",
         "currentFloor":0,
+        "direction": "",
     }
  liftState.push(eachliftDetail);
 });  
@@ -16,9 +18,9 @@ for(let i=16;i>0;i--){
     newElement.innerHTML = ` 
     <div class="perFloor" id=${i}>
    
-    <button id=${i} class="upButton" onclick="handleMovement(${i})">UP</button>
+    <button id=${i} class="upButton" onclick="handleMovement(${i},'upButton')">UP</button>
     
-    <button class="downButton" onclick="handleMovement(${i})">DOWN</button>
+    <button class="downButton" onclick="handleMovement(${i},'downButton')">DOWN</button>
     <div>Floor Number: ${i}</div>
     </div>`
     ;     
@@ -37,48 +39,47 @@ for(let i=16;i>0;i--){
         newLifts.appendChild(newElement); 
         }
         
-
-    const handleMovement=(floorcalled)=>{
-     if(number>4)
+    const handleMovement=(floorcalled ,buttonPressed)=>{
+      if(number>4)
         number=1;
-
+         
+         for(let i=0;i<liftState.length ;i++){
+ 
+          if ( liftState[i].currentFloor==floorcalled &&  liftState[i].direction==buttonPressed  ){  
+               return ;
+              } 
     
 
-    for(let i=0;i<liftState.length;i++){
-    if (liftState[i].currentFloor==floorcalled){     // 
-          return  ;
-    }}
-    
-
+        }
+     
     const lifts= document.querySelector(`.elevators`); 
     const singleLift = lifts.querySelector(`[id="${number}"]`);
 
-    singleLift.addEventListener('click',liftMovementTrack(number,"moving",floorcalled)); 
+    singleLift.addEventListener('click',liftMovementTrack(number,"moving",floorcalled,`${buttonPressed}`)); 
     
     singleLift.style.transform= `translateY(-${floorcalled*100}px)`;  
     singleLift.style.transition=`transform 2s ease`; 
     setTimeout (()=>{
-    liftMovementTrack(number,"idle",floorcalled);
+    liftMovementTrack(number,"idle",floorcalled,`${buttonPressed}`);
+    number++;  
    },2000);
-   number++;  
-  
-
 }
 
 
-   const liftMovementTrack=(liftnumber,state,currentFloor)=>{
+   const liftMovementTrack=(liftnumber,state,currentFloor,buttonPressed)=>{
     const updatedLift = liftState.map((lift) => {
         if (lift.id === liftnumber) {
           return {
             ...lift,               
             state: state,          
-            currentFloor: currentFloor 
+            currentFloor: currentFloor ,
+            direction:buttonPressed,
           };
         }
         return lift; 
       });
        liftState=updatedLift;
-      console.log(updatedLift);
+      console.log(updatedLift); 
 
    }
    
